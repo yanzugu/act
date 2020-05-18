719e2f91588498b306235a5227aef719d5dfe501
yanzugu <yanzugu@gmail.com>
2020-05-18 17:36:44 +0800

    login

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from views import register
+from .views import register, login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html')),
     path('register/', register, name='register'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login', login, name='login'),
+    path('accounts/logout', logout),
 ]
---
+++ mysite/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm # 新增
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        print("Errors", form.errors)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, 'registration/register.html', {'form':form})
+    else:
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'registration/register.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return redirect('/')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+    return redirect('/')
